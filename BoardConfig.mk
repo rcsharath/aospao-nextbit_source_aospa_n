f1aca2d12231994180ca2e15677333074a2482ee
Jake Weinstein <xboxlover360@gmail.com>
2016-10-18 00:25:08 -0400

    robin: cleanup dex-preoptimization configuration
    * WITH_DEXPREOPT_PIC is a regression for performance
    * no-stripping creates excessively large images

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,16 +118,8 @@ TARGET_PER_MGR_ENABLED := true
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 # Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-    WITH_DEXPREOPT_PIC := true
-    ifneq ($(TARGET_BUILD_VARIANT),user)
-      # Retain classes.dex in APK's for non-user builds
-      DEX_PREOPT_DEFAULT := nostripping
-    endif
-  endif
-endif
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_PIC := false
 
 # Enable sensor multi HAL
 USE_SENSOR_MULTI_HAL := true
