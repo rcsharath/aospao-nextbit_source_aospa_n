069cea78ea2df915e8cce35d070efca4b13858bb
Alex Naidis <alex.naidis@linux.com>
2016-10-19 15:10:05 +0200

    robin: build audio daemon
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- robin.mk
+++ robin.mk
@@ -67,12 +67,16 @@ endif
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
+
+
 # Audio configuration file
 -include $(TOPDIR)hardware/qcom/audio/configs/msm8992/msm8992.mk
 
 PRODUCT_COPY_FILES += \
     device/nextbit/robin/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/nextbit/robin/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+PRODUCT_PACKAGES += audiod
 
 # WLAN driver configuration files
 PRODUCT_COPY_FILES += \
