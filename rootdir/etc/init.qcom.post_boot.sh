4c99d949731da43364e184fcc56e90f5afedcd59
Alex Naidis <alex.naidis@linux.com>
2016-10-21 15:38:24 +0200

    robin: rework power configuration
    * Switch to static target loads value
      for small cores, the current definition
      seems to be not optimal
    * Lower go_hispeed_load of big cores
      -> so it can scale up in time
    * Lower scaling_min_freq to 460000
      to make it more dynamic
    * Adjust perfd prop to match this
    * Higher input boost frequency to
      960000, 787200 is not enough for
      a smooth experience in all cases.
    * Add missing max_cpus setting for core_ctl
    * Update core ctl busy_up_thres and task_thres
      to make it harder to online cpu5
    * Update sched up/downmigrate values to increase
      usage of cpu4.
    * Lower sched_freq_inc/dec_notify
      to 300000 to make the scheduler
      notify the governor more often.
    
    Change-Id: Ib27261864865c6f549b6aed279362658bb0898b2
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -818,7 +818,7 @@ case "$target" in
         echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
         echo 960000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
         echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-        echo "65 460800:75 960000:80" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+        echo 85 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
         echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
         echo 80000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
         echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
@@ -831,14 +831,14 @@ case "$target" in
         echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
         echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
         echo 19000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-        echo 99 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+        echo 95 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
         echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
         echo 1248000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
         echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
         echo "70 960000:80 1248000:85" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
         echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
         echo 80000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
-        echo 633600 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+        echo 460000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
         # restore A57's max
         cat /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
         # re-enable thermal and BCL hotplug
@@ -865,26 +865,27 @@ case "$target" in
         # Restore CPU 4 max freq from msm_performance
         echo "4:4294967295 5:4294967295" > /sys/module/msm_performance/parameters/cpu_max_freq
         # input boost configuration
-        echo 0:787200 > /sys/module/cpu_boost/parameters/input_boost_freq
+        echo 0:960000 > /sys/module/cpu_boost/parameters/input_boost_freq
         echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
 
         # core_ctl module
         insmod /system/lib/modules/core_ctl.ko
+        echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
         echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
-        echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
+        echo 70 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
         echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
         echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
         echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
-        echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
+        echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
         # Setting b.L scheduler parameters
         echo 1 > /proc/sys/kernel/sched_migration_fixup
         echo 30 > /proc/sys/kernel/sched_small_task
         echo 20 > /proc/sys/kernel/sched_mostly_idle_load
         echo 3 > /proc/sys/kernel/sched_mostly_idle_nr_run
-        echo 99 > /proc/sys/kernel/sched_upmigrate
-        echo 85 > /proc/sys/kernel/sched_downmigrate
-        echo 400000 > /proc/sys/kernel/sched_freq_inc_notify
-        echo 400000 > /proc/sys/kernel/sched_freq_dec_notify
+        echo 80 > /proc/sys/kernel/sched_upmigrate
+        echo 60 > /proc/sys/kernel/sched_downmigrate
+        echo 300000 > /proc/sys/kernel/sched_freq_inc_notify
+        echo 300000 > /proc/sys/kernel/sched_freq_dec_notify
         #enable rps static configuration
         echo 8 >  /sys/class/net/rmnet_ipa0/queues/rx-0/rps_cpus
         for devfreq_gov in /sys/class/devfreq/qcom,cpubw*/governor
@@ -1145,7 +1146,7 @@ case "$target" in
     "msm8994" | "msm8992")
         rm /data/system/perfd/default_values
         setprop ro.min_freq_0 384000
-        setprop ro.min_freq_4 384000
+        setprop ro.min_freq_4 460000
         start perfd
     ;;
     "apq8084")
